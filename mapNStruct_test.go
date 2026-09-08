2bfcc5f9be3917942a7dc00111038463669fd299
L1mus <ali.firefox@yahoo.co.id>
2026-09-08 11:41:32 +0700

    feat: struct type

--- mapNStruct_test.go
+++ mapNStruct_test.go
@@ -8,6 +8,7 @@ import (
 
 func TestMapNStruct(t *testing.T) {
 	mapType()
+	structType()
 }
 
 func mapType() {
@@ -45,5 +46,94 @@ func mapType() {
 	} else {
 		fmt.Printf("3. %s not found\n", sym)
 	}
+
+}
+
+type Product struct {
+	ID          int
+	Name        string
+	Price       float64
+	Stock       int
+	IsAvailable bool
+}
+
+func NewProduct(id int, name string, price float64, stock int, available bool) *Product {
+	return &Product{
+		ID:          id,
+		Name:        name,
+		Price:       price,
+		Stock:       stock,
+		IsAvailable: available,
+	}
+}
+
+func (p *Product) UpdateAvailability() {
+	if p.Stock > 0 {
+		p.IsAvailable = true
+	} else {
+		p.IsAvailable = false
+	}
+}
+
+func (p *Product) BuyItem() {
+	if p.Stock > 0 {
+		p.Stock--
+	} else {
+		p.Stock = 0
+	}
+
+}
+
+func structType() {
+
+	item1 := NewProduct(1, "Sugar", 5.00, 10, true)
+	item2 := NewProduct(2, "Garlic", 0.40, 100, true)
+	item3 := NewProduct(3, "Carrot", 0.70, 50, true)
+	item4 := NewProduct(4, "Fluor", 0.30, 1, true)
+
+	products := make([]Product, 0)
+	products = append(products, *item1, *item2, *item3, *item4)
+
+	for _, v := range products {
+		fmt.Printf(`
+		Nama Produk : %s
+		harga : $%0.2f
+		stock : %d
+		status ketersediaan : %v
+		`, v.Name, v.Price, v.Stock, v.IsAvailable)
+	}
+
+	simulationStockZero(&products[3])
+	simulationStockZero(&products[3])
+
+	var totalInventaris float64
+
+	for _, v := range products {
+		totalInventaris += v.Price * float64(v.Stock)
+	}
+
+	fmt.Printf("\njumlah product : %d \nTotal Inventaris semua product adalah : $%0.2f\n", len(products), totalInventaris)
+}
+
+func simulationStockZero(item *Product) {
+	fmt.Printf("\nBefore buy : %v", item)
+
+	//check availablelity
+	available := item.Stock > 0
+
+	if !available {
+		fmt.Println("\nItem out of stock")
+		return
+	}
+
+	//buy simulation
+	item.BuyItem()
+
+	item.UpdateAvailability()
+
+	fmt.Printf("\nAfter buy : %v", item)
+
+	//asumsiton succes buy
+	fmt.Println("\nThanks for buying, have a nice day")
 
 }
